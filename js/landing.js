d3adcd38792a0160c23e92906cb864cdf3a9b535
Kaung Khant Zaw <kaungkz93@gmail.com>
2019-11-22 14:38:51 +0630

    landing page js finished

---
+++ js/landing.js
@@ -0,0 +1,30 @@
+const goDownBtn = document.querySelector(".go-down");
+
+function smoothScroll(target, duration) {
+  const goal = document.querySelector(target);
+  const targetPosition = goal.getBoundingClientRect().top;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  let startTime = null;
+
+  function Scrollanimation(time) {
+    if (startTime === null) {
+      startTime = time;
+    }
+    const timeElapsed = time - startTime;
+    const run = calculation(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+
+    if (timeElapsed < duration) {
+      requestAnimationFrame(Scrollanimation);
+    }
+  }
+  function calculation(t, b, c, d) {
+    t /= d;
+    return -c * t * (t - 2) + b;
+  }
+  requestAnimationFrame(Scrollanimation);
+}
+goDownBtn.addEventListener("click", () => {
+  smoothScroll(".description", 1000);
+});
